3f9a5a67fe147ea261bae1160266fb340feb2bc2
Ubuntu <ubuntu@ip-172-31-82-155.ec2.internal>
2020-04-25 14:34:48 +0000

    Added High Availability With Zookeeper

--- slave/app.py
+++ slave/app.py
@@ -3,89 +3,128 @@ import time
 import json
 import sqlite3
 import threading
+import os
 
-threads = []
+if(str(os.environ['IS_MASTER']) == "False"):
+	threads = []
 
-class Response_Object:
-	def __init__(self,response,corr_id):
-		self.response = response
-		self.corr_id = corr_id
+	class Response_Object:
+		def __init__(self,response,corr_id):
+			self.response = response
+			self.corr_id = corr_id
 
-sleepTime = 20
-print(' [*] Sleeping for ', sleepTime, ' seconds.')
-time.sleep(sleepTime)
+	if(str(os.environ['IS_FIRST_SLAVE']) == "True"):
+		sleepTime = 40
+		print(' [*] Sleeping for ', sleepTime, ' seconds.')
+		time.sleep(sleepTime)
 
-print(' [*] Connecting to server ...')
+	print(' [*] Connecting to server ...')
 
-print(' [*] Waiting for messages.')
+	print(' [*] Waiting for messages.')
 
 
-def callback_read(ch, method, properties, body):
+	def callback_read(ch, method, properties, body):
+		conn = sqlite3.connect('database.db')
+		db = conn.cursor()
+		print(" [x] Received %s" % body)
+		#content = json.loads(body)
+		#cmd = content['query']
+		#req_no = content['corr_id']
+		cmd = body.decode()
+		result = db.execute(cmd)
+		response = result.fetchall()
+		print(response)
+		#responseobj = {"corr_id" : req_no, "response" : response}
+		#connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+		#response_channel = connection.channel()
+		#response_channel.queue_declare(queue='RESPONSEQ', durable=True)
+		ch.basic_publish(exchange='',
+	                     routing_key=properties.reply_to,
+	                     properties=pika.BasicProperties(correlation_id = properties.correlation_id),
+	                     body=json.dumps(response))
+		ch.basic_ack(delivery_tag=method.delivery_tag)
+		conn.close()
+		#connection.close()
+
+
+	def callback_sync(ch, method, properties, body):
+		conn = sqlite3.connect('database.db')
+		db = conn.cursor()
+		print(" [x] Received %s" % body)
+		content = body.decode()
+		result = db.execute(content)
+		conn.commit()
+		conn.close()
+		ch.basic_ack(delivery_tag=method.delivery_tag)
+
+	def read_consume():
+		connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq',heartbeat=600))
+		read_channel = connection.channel()
+		read_channel.queue_declare(queue='READQ', durable=True)
+		read_channel.basic_qos(prefetch_count=1)
+		read_channel.basic_consume(queue='READQ', on_message_callback=callback_read)
+		read_channel.start_consuming()
+
+	def sync_consume():
+		connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq',heartbeat=600))
+		sync_channel = connection.channel()
+		sync_channel.exchange_declare(exchange='SYNCEXCHANGE', exchange_type='fanout')
+		result = sync_channel.queue_declare(queue='', durable=True)
+		queue_name = result.method.queue
+		sync_channel.queue_bind(exchange='SYNCEXCHANGE', queue=queue_name)
+		#sync_channel.basic_qos(prefetch_count=1)
+		sync_channel.basic_consume(queue=queue_name, on_message_callback=callback_sync)
+		sync_channel.start_consuming()
+
+	def manager():
+		t1 = threading.Thread(target=read_consume)
+		t1.daemon = True
+		threads.append(t1)
+		t1.start()  
+
+		t2 = threading.Thread(target=sync_consume)
+		t2.daemon = True
+		threads.append(t2)
+
+
+		t2.start()
+		for t in threads:
+			t.join()
+
+	manager()
+else:
 	conn = sqlite3.connect('database.db')
 	db = conn.cursor()
-	print(" [x] Received %s" % body)
-	content = json.loads(body)
-	cmd = content['query']
-	req_no = content['corr_id']
-	result = db.execute(cmd)
-	response = result.fetchall()
-	print(response)
-	responseobj = {"corr_id" : req_no, "response" : response}
-	connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-	response_channel = connection.channel()
-	response_channel.queue_declare(queue='RESPONSEQ', durable=True)
-	response_channel.basic_publish(
-		exchange='',
-		routing_key='RESPONSEQ',
-		body=json.dumps(responseobj),
-		properties=pika.BasicProperties(
-			delivery_mode=2,  # make message persistent
-		))
-	ch.basic_ack(delivery_tag=method.delivery_tag)
-	conn.close()
-	connection.close()
 
+	sleepTime = 40
+	print(' [*] Sleeping for ', sleepTime, ' seconds.')
+	time.sleep(sleepTime)
 
-def callback_sync(ch, method, properties, body):
-	conn = sqlite3.connect('database.db')
-	db = conn.cursor()
-	print(" [x] Received %s" % body)
-	content = body.decode()
-	result = db.execute(content)
-	conn.commit()
-	conn.close()
-	ch.basic_ack(delivery_tag=method.delivery_tag)
-
-def read_consume():
+	print(' [*] Connecting to server ...')
 	connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq',heartbeat=600))
-	read_channel = connection.channel()
-	read_channel.queue_declare(queue='READQ', durable=True)
-	read_channel.basic_qos(prefetch_count=1)
-	read_channel.basic_consume(queue='READQ', on_message_callback=callback_read)
-	read_channel.start_consuming()
-
-def sync_consume():
-	connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq',heartbeat=600))
+	write_channel = connection.channel()
+	write_channel.queue_declare(queue='WRITEQ', durable=True)
 	sync_channel = connection.channel()
-	sync_channel.queue_declare(queue='SYNCQ', durable=True)
-	sync_channel.basic_qos(prefetch_count=1)
-	sync_channel.basic_consume(queue='SYNCQ', on_message_callback=callback_sync)
-	sync_channel.start_consuming()
+	sync_channel.exchange_declare(exchange='SYNCEXCHANGE', exchange_type='fanout')
 
-def manager():
-	t1 = threading.Thread(target=read_consume)
-	t1.daemon = True
-	threads.append(t1)
-	t1.start()  
-
-	t2 = threading.Thread(target=sync_consume)
-	t2.daemon = True
-	threads.append(t2)
+	print(' [*] Waiting for messages.')
 
 
-	t2.start()
-	for t in threads:
-		t.join()
+	def callback_write(ch, method, properties, body):
+		print(" [x] Received %s" % body)
+		content = body.decode()
+		result = db.execute(content)
+		conn.commit()
+		#conn.close()
+		sync_channel.basic_publish(
+			exchange='SYNCEXCHANGE',
+			routing_key='',
+			body= content,
+			properties=pika.BasicProperties(
+				delivery_mode=2,  # make message persistent
+			))
+		ch.basic_ack(delivery_tag=method.delivery_tag)
 
-manager()
-
+	#write_channel.basic_qos(prefetch_count=1)
+	write_channel.basic_consume(queue='WRITEQ', on_message_callback=callback_write)
+	write_channel.start_consuming()
